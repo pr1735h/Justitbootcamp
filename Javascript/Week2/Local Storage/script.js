3bb168e7413e4cdce43c23237a062f601b414668
pr1735h <150253096+pr1735h@users.noreply.github.com>
2023-12-06 15:00:56 +0000

    5465

---
+++ Javascript/Week2/Local Storage/script.js
@@ -0,0 +1,2 @@
+const title = document.getElementById("title");
+console.log(title);
--- Javascript/Week2/d2taskscript.js
+++ Javascript/Week2/d2taskscript.js
@@ -1,18 +1,27 @@
+document.body.style.backgroundColor = "black";
+const bgGradient = "linear-gradient( 0deg, rgba(18,134,155,0.5046393557422969) 0%, rgba(155, 97, 100, 0.799) 50%, rgba(43, 22, 22, 0.527) 100%)";
+document.body.style.backgroundImage = bgGradient;
+
 //Task1:
 const imgButt = document.getElementById("imgButt");
 const puppy = document.getElementById("puppy");
-imgButt.style.backgroundColor = "green";
+imgButt.style.backgroundImage = bgGradient;
 imgButt.style.width = "100px";
 imgButt.style.height = "100px";
+imgButt.style.margin = "50px";
 
 imgButt.addEventListener("click", function () {
     if (puppy.style.display === "none") {
-        puppy.style.display = "block";
-        imgButt.style.backgroundColor = "crimson";
+        puppy.style.display = "inline-block";
+        imgButt.style.backgroundColor = "white";
+        imgButt.style.position = "relative";
+        imgButt.style.top = "100px";
     }
     else {
         puppy.style.display = "none";
         imgButt.style.backgroundColor = "lightblue";
+        imgButt.style.position = "relative";
+        imgButt.style.top = "250px";
     }
 });
 
@@ -20,8 +29,10 @@ imgButt.addEventListener("click", function () {
 const form1 = document.getElementById("form1");
 const input1 = document.getElementById("input1");
 const submit = document.getElementById("submit");
+submit.style.backgroundImage = bgGradient;
 submit.style.width = "100px";
 submit.style.height = "100px";
+form1.style.marginTop = "200px";
 
 submit.addEventListener("click", function () {
     event.preventDefault();
